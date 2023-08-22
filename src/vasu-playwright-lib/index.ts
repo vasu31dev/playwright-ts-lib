11555f3d0762b5aef300420e8daed95c1369498a
vasu31dev <vasu31dev@gmail.com>
2023-08-21 20:17:55 -0400

    Exporting relative paths for all the utilities

--- src/vasu-playwright-lib/index.ts
+++ src/vasu-playwright-lib/index.ts
@@ -1,3 +1,6 @@
-export * from './utils';
+export * from './utils/action-utils';
+export * from './utils/element-utils';
+export * from './utils/locator-utils';
+export * from './utils/page-utils';
 export * from './constants';
 export * from './types';
